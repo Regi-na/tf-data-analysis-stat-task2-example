2203cc6a19995bdba76b901b48be97a1763197a7
Regi-na <64543329+Regi-na@users.noreply.github.com>
2023-04-06 13:43:34 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -15,5 +15,5 @@ def solution(p: float, x: np.array) -> tuple:
     n = len(x)
     s2 = np.var(x, ddof=1)
     l = n - 1  
-    return x.mean() - np.sqrt(np.var(x)) * norm.ppf(1 - alpha / 2) / np.sqrt(len(x)), \
-           x.mean() - np.sqrt(np.var(x)) * norm.ppf(alpha / 2) / np.sqrt(len(x))
+    return x.std() - np.sqrt(np.var(x)) * norm.ppf(1 - alpha / 2) / np.sqrt(len(x)), \
+           x.std() - np.sqrt(np.var(x)) * norm.ppf(alpha / 2) / np.sqrt(len(x))
