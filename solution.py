27d08496638c6bde62c1ce9f331dd2f5509e1698
Regi-na <64543329+Regi-na@users.noreply.github.com>
2023-04-07 17:45:40 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -13,9 +13,11 @@ def solution(p: float, x: np.array) -> tuple:
     # Не меняйте название функции и её аргументы
     alpha = 1 - p
     n = len(x)
-    s2 = np.var(x, ddof=1)
+    s2 = np.var(x)
     l = n - 1  
     #return s2 * np.sqrt(l / chi2.ppf(1 - alpha / 2, l)), \
           # s2 * np.sqrt(l / chi2.ppf(alpha / 2, l))
-    return np.sqrt(s2**2 * n / chi2.ppf(1 - alpha / 2, n)), \
-           np.sqrt(s2**2 * n / chi2.ppf(alpha / 2, n))
+    #return np.sqrt(s2**2 * n / chi2.ppf(1 - alpha / 2, n)), \
+     #      np.sqrt(s2**2 * n / chi2.ppf(alpha / 2, n))
+    return np.std(x)/(32**0.5) * l /chi2.ppf(alpha / 2, df = l) * np.mean(x), \
+           np.std(x)/(32**0.5) * l / chi2.ppf(1-alpha / 2, df = l) * np.mean(x)
